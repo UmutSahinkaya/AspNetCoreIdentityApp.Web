bcde5579403d54509cb7d9a0e7022072a564e39a
umutSahinkaya <umutsahinkaya1@gmail.com>
2024-09-30 14:26:04 +0300

    CustomErrorDescriber

--- Extensions/StartupExtensions.cs
+++ Extensions/StartupExtensions.cs
@@ -1,23 +1,23 @@
 ﻿using AspNetCoreIdentityApp.Web.CustomValidations;
+using AspNetCoreIdentityApp.Web.Localizations;
 using AspNetCoreIdentityApp.Web.Models;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 
-namespace AspNetCoreIdentityApp.Web.Extensions
+namespace AspNetCoreIdentityApp.Web.Extensions;
+
+public static class StartupExtensions
 {
-    public static class StartupExtensions
+    public static void AddIdentityWithExt(this IServiceCollection services)
     {
-        public static void AddIdentityWithExt(this IServiceCollection services)
+        services.AddIdentity<AppUser, AppRole>(options =>
         {
-            services.AddIdentity<AppUser, AppRole>(options =>
-            {
-                options.User.RequireUniqueEmail = true;
-                options.User.AllowedUserNameCharacters = "abcdefghijklmnoprstuvwxyz123456789_.";
-                options.Password.RequiredLength = 6;
-                options.Password.RequireNonAlphanumeric = false;
-                options.Password.RequireUppercase = false;
-                options.Password.RequireLowercase = true;
-                options.Password.RequireDigit = false;
-            }).AddUserValidator<UserValidator>().AddPasswordValidator<PasswordValidator>().AddEntityFrameworkStores<AppDbContext>();
-        }
+            options.User.RequireUniqueEmail = true;
+            options.User.AllowedUserNameCharacters = "abcdefghijklmnoprstuvwxyz123456789_.";
+            options.Password.RequiredLength = 6;
+            options.Password.RequireNonAlphanumeric = false;
+            options.Password.RequireUppercase = false;
+            options.Password.RequireLowercase = true;
+            options.Password.RequireDigit = false;
+        }).AddErrorDescriber<LocalizationIdentityErrorDescriber>().AddUserValidator<UserValidator>().AddPasswordValidator<PasswordValidator>().AddEntityFrameworkStores<AppDbContext>();
     }
 }
---
+++ Localizations/LocalizationIdentityErrorDescriber.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.VisualStudio.Web.CodeGenerators.Mvc.Templates.BlazorIdentity.Pages.Manage;
+
+namespace AspNetCoreIdentityApp.Web.Localizations
+{
+    public class LocalizationIdentityErrorDescriber:IdentityErrorDescriber
+    {
+        public override IdentityError DuplicateUserName(string userName)
+        {
+            return new() { Code = "DuplicateUserName", Description = $"{userName} daha önce başkası tarafından alınmıştır." };
+            //return base.DuplicateUserName(userName);
+        }
+        public override IdentityError DuplicateEmail(string email)
+        {
+            return new() { Code = "DuplicateEmail", Description = $"{email} daha önce başkası tarafından alınmıştır." };
+            //return base.DuplicateEmail(email);
+        }
+        public override IdentityError PasswordTooShort(int length)
+        {
+            return new() { Code = "PasswordTooShort", Description = $"Şifre en az 6 karakterli olmalıdır." };
+            //return base.PasswordTooShort(length);
+        }
+    }
+}
