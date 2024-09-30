c1da824f908f57a65fed78dcce29413adf8b2999
umutSahinkaya <umutsahinkaya1@gmail.com>
2024-09-30 17:04:01 +0300

    Çıkış yap action method’un kodlanması-1

---
+++ Controllers/MembersController.cs
@@ -0,0 +1,22 @@
+﻿using AspNetCoreIdentityApp.Web.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetCoreIdentityApp.Web.Controllers
+{
+    public class MembersController : Controller
+    {
+        private readonly SignInManager<AppUser> _signInManager;
+
+        public MembersController(SignInManager<AppUser> signInManager)
+        {
+            _signInManager = signInManager;
+        }
+
+        public async Task<IActionResult> Logout()
+        {
+            await _signInManager.SignOutAsync();
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
